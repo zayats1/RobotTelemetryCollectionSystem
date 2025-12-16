55dcf11e9c7a173d54b2bbae0b7258dcf727777f
zayats1 <zaiats.bogdan@gmail.com>
2025-12-16 21:54:47 +0200

    Telemetry collectiuon system; begin

---
+++ frontend/src/app.rs
@@ -0,0 +1,70 @@
+use crate::visualizer::Visualizer;
+use leptos::prelude::*;
+use leptos_meta::{provide_meta_context, MetaTags, Stylesheet, Title};
+use leptos_router::{
+    components::{Route, Router, Routes},
+    StaticSegment,
+};
+use crate::robot_map::RobotMap;
+
+pub fn shell(options: LeptosOptions) -> impl IntoView {
+    view! {
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <AutoReload options=options.clone() />
+                <HydrationScripts options/>
+                <MetaTags/>
+            </head>
+            <body>
+                <App/>
+            </body>
+        </html>
+    }
+}
+
+#[component]
+pub fn App() -> impl IntoView {
+    // Provides context that manages stylesheets, titles, meta tags, etc.
+    provide_meta_context();
+
+    view! {
+        // injects a stylesheet into the document <head>
+        // id=leptos means cargo-leptos will hot-reload this stylesheet
+        <Stylesheet id="leptos" href="/pkg/telemetry-collector.css"/>
+
+        // sets the document title
+        <Title text="Welcome to Leptos"/>
+        <nav>
+          <a href="/Visualizer/">Visualizer</a>
+          <a href="/HomePage/">HomePage</a>
+          <a href="/RobotMap/">RobotMap</a>
+        </nav>
+        // content for this welcome page
+        <Router>
+            <main>
+                <Routes fallback=|| "Page not found.".into_view()>
+                    <Route path=StaticSegment("") view=HomePage/>
+                    <Route path=StaticSegment("HomePage") view=HomePage/>
+                    <Route path=StaticSegment("Visualizer") view=Visualizer/>
+                    <Route path=StaticSegment("RobotMap") view=RobotMap/>
+                </Routes>
+            </main>
+        </Router>
+    }
+}
+
+/// Renders the home page of your application.
+#[component]
+fn HomePage() -> impl IntoView {
+    // Creates a reactive value to update the button
+    let count = RwSignal::new(0);
+    let on_click = move |_| *count.write() += 1;
+
+    view! {
+        <h1>"Welcome to Leptos!"</h1>
+        <button on:click=on_click>"Click Me: " {count}</button>
+    }
+}
