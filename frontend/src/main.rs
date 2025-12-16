4e861a4c605f838857d165a7f4cc51cdb08bf197
zayats1 <zaiats.bogdan@gmail.com>
2025-12-16 22:22:31 +0200

    working project start

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -6,7 +6,7 @@ async fn main() {
     use leptos::logging::log;
     use leptos::prelude::*;
     use leptos_axum::{generate_route_list, LeptosRoutes};
-    use telemetry_collector::app::*;
+    use frontend::app::*;
 
     let conf = get_configuration(None).unwrap();
     let addr = conf.leptos_options.site_addr;
