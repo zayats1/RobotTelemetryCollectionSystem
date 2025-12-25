1eee94a7956348cdc588e3c2e5938870700a44d7
zayats1 <zaiats.bogdan@gmail.com>
2025-12-25 20:23:28 +0200

    basic receiver

--- server/src/receiver.rs
+++ server/src/receiver.rs
@@ -1,6 +1,8 @@
+use axum::Json;
 use robot_data::{RobotInfo, RobotInfoParsingError};
 
-async fn receive_telemetry(address:String) -> Result<RobotInfo,RobotInfoParsingError>   {
-
-
+async fn receive_telemetry(info_type:Json<String>,robot_data:Json<String>) -> Result<RobotInfo,RobotInfoParsingError>   {
+    let _ = format!("Received type: {}, data: {}", info_type.to_string(),robot_data.to_string());
+    let parsed = RobotInfo::parse(info_type.as_str(), robot_data.as_str())?;
+    Ok(parsed)
 }
