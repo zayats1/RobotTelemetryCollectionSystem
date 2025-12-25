bd8e6cf7e0c473aaf695bf8359191ecdf8ba9fb8
zayats1 <zaiats.bogdan@gmail.com>
2025-12-25 20:24:31 +0200

    tuple input

--- server/src/receiver.rs
+++ server/src/receiver.rs
@@ -1,7 +1,7 @@
 use axum::Json;
 use robot_data::{RobotInfo, RobotInfoParsingError};
 
-async fn receive_telemetry(info_type:Json<String>,robot_data:Json<String>) -> Result<RobotInfo,RobotInfoParsingError>   {
+async fn receive_telemetry((info_type,robot_data):(Json<String>,Json<String>)) -> Result<RobotInfo,RobotInfoParsingError>   {
     let _ = format!("Received type: {}, data: {}", info_type.to_string(),robot_data.to_string());
     let parsed = RobotInfo::parse(info_type.as_str(), robot_data.as_str())?;
     Ok(parsed)
