78adc21192b997824225196fc2ed5388d6e46a91
zayats1 <zaiats.bogdan@gmail.com>
2025-12-28 16:57:39 +0200

    server receives telemetry now

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,11 +5,13 @@ use axum::{
     routing::get,
     Router,
 };
+use crate::receiver::receive_telemetry;
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", get(|| async { "telemetry collector" })
+        .post(|data| receive_telemetry(data)));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
--- server/src/receiver.rs
+++ server/src/receiver.rs
@@ -1,7 +1,7 @@
 use axum::Json;
 use robot_data::RobotInfo;
 
-async fn receive_telemetry(data: Json<RobotInfo>) -> Json<RobotInfo>   {
+pub async fn receive_telemetry(data: Json<RobotInfo>) -> Json<RobotInfo>   {
     let _ = format!("Received data: {:?}",data.clone() );
     data
 }
