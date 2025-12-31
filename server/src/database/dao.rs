22b98d3f0387bdcb5db52e65d34ca24a0f233c76
zayats1 <zaiats.bogdan@gmail.com>
2025-12-31 18:39:15 +0200

    movement_info dao

--- server/src/database/dao.rs
+++ server/src/database/dao.rs
@@ -1,17 +1,14 @@
 
-use libsql::{de,params, Connection};
-use robot_data::robot_info::{BasicInfo, MovementInfo};
+use libsql::{de, params, Connection, Error, Row};
+use robot_data::robot_info::{BasicInfo, MovementInfo, Vec3};
 use robot_data::RobotInfo;
 
-pub    trait DAO {
+pub  trait DAO {
    async fn insert_to_db(&self, conn: &Connection) ->  libsql::Result<u64>;
    async  fn get_by_id(id: String, conn: &Connection) -> Result<Vec<Self>, libsql::Error>
     where
         Self: Sized;
 }
-
-
-
 
 
 impl DAO for BasicInfo {
@@ -49,20 +46,42 @@ impl DAO for MovementInfo {
         ).await
     }
 
-    async fn get_by_id(id: u64, conn: &Connection) -> Result<Vec<Self>, libsql::Error>
+    async fn get_by_id(id: String, conn: &Connection) -> Result<Vec<Self>, libsql::Error>
     where
         Self: Sized,
     {
-        let stmt = conn.prepare("SELECT id, robot_type FROM  BasicInfo WHERE id = (?)").await?;
+        let stmt = conn.prepare("SELECT id, robot_type FROM  Movement WHERE id = (?)").await?;
         let mut rows = stmt
             .query([id])
             .await?;
-        let mut data:Vec<BasicInfo> = vec![];
+        let mut data:Vec<MovementInfo> = vec![];
 
         while let Some(row )  = rows.next().await?{
-            data.push(de::from_row::<BasicInfo>(&row).map_err(|e| libsql::Error::InvalidParserState(e.to_string()))?);
+            let info = MovementInfo{
+                id : row.get(0)?,
+                speed: Vec3{
+                    x:  sql_val_to_f32(&row, 1)?,
+                    y:  sql_val_to_f32(&row, 2)?,
+                    z:  sql_val_to_f32(&row, 3)?,
+                },
+                acc:  Vec3{
+                    x:  sql_val_to_f32(&row, 4)?,
+                    y:  sql_val_to_f32(&row, 5)?,
+                    z:  sql_val_to_f32(&row, 6)?,
+                },
+                timestamp: Default::default(),
+            };
+
+
+            data.push(info);
         }
         Ok(data)
 
     }
 }
+
+fn sql_val_to_f32(row: &Row, idx:i32) -> Result<f32, Error> {
+    row.get_str(1)?.parse::<f32>().map_err(|e| libsql::Error::InvalidParserState(e.to_string()))
+}
+
+
