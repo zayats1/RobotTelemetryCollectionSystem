2e8a425035e954a5ca4f9f3f1246cd24fc328b27
zayats1 <zaiats.bogdan@gmail.com>
2025-12-31 13:13:00 +0200

    remove unwraps

--- server/src/main.rs
+++ server/src/main.rs
@@ -9,12 +9,13 @@ use axum::routing::post;
 use crate::receiver::receive_telemetry;
 
 #[tokio::main]
-async fn main() {
+async fn main()  -> Result<(), std::io::Error> {
     // build our application with a single route
     let app = Router::new().route("/", get(|| async { "telemetry collector" }))
         .route("/telemetry",post(|data| receive_telemetry(data)));
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
+
+    axum::serve(listener, app).await
 }
