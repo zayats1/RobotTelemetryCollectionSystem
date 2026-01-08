70196174c18cfc6c62c858407b36bb9506b5a03f
zayats1 <zaiats.bogdan@gmail.com>
2026-01-08 22:11:06 +0200

    fetch

--- client_frontend/src/app_model/mod.rs
+++ client_frontend/src/app_model/mod.rs
@@ -1,6 +1,7 @@
 use reqwest::Client;
 use robot_data::{RobotInfo, RobotInfoType};
 use std::sync::Arc;
+use robot_data::robot_info::BasicInfo;
 
 struct AppModel {
     client: Arc<reqwest::Client>,
@@ -12,6 +13,23 @@ impl AppModel {
             client: Arc::new(Client::new()),
         }
     }
+
+pub async fn fetch_robots(
+    &self,
+) -> Result<Vec<BasicInfo>, String> {
+    // Todo: Url
+    let info = self
+        .client
+        .get(
+            "127.0.0.1:3000/robots/"
+        )
+        .send()
+        .await
+        .map_err(|err| err.to_string())?;
+    info.json::<Vec<BasicInfo>>()
+        .await
+        .map_err(|err| err.to_string())
+}
     pub async fn fetch_info(
         &self,
         id: String,
@@ -22,7 +40,7 @@ impl AppModel {
         let info = self
             .client
             .get(format!(
-                "127.0.0.1:3000/info/?id={}&info_type={}",
+                "127.0.0.1:3000/info_from/?id={}&info_type={}",
                 id, info_type
             ))
             .send()
