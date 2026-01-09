66a42ecc96c1f492c4b8903855469780aa73755b
zayats1 <zaiats.bogdan@gmail.com>
2026-01-09 22:58:41 +0200

    hamster!

--- client_frontend/src/pages/about.rs
+++ client_frontend/src/pages/about.rs
@@ -1,14 +1,26 @@
 use leptos::{component, view, IntoView};
+use leptos::attr::{alt, src};
+use leptos::html::img;
 use leptos::prelude::*;
+
+
 #[component]
 pub fn AboutPage() -> impl IntoView {
     // Creates a reactive value to update the button
+    // Creates a reactive value to update the button
+    let count = RwSignal::new(0);
 
+    let on_click = move |_| *count.write() = (count.get() as u64).wrapping_add(1);
     view! {
+        <h1>"About"</h1>
         <div class="about">
-            <h1>"About"</h1>
             <p>"Robot telemetry collection system"</p>
             <p>"Version: 0.1"</p>
+         <button on:click=on_click class="hamster-tap">
+         <p> "Tap on the hamster: "</p>
+            <img src= "/pictures/hamster.png" width="42pt" height="42pt"  alt="se la vi"/>
+        </button>
+           <p> "Bucks: " { move || count.get()} "$" </p>
         </div>
     }
 }
--- client_frontend/src/pages/home.rs
+++ client_frontend/src/pages/home.rs
@@ -14,17 +14,12 @@ use crate::pages::test_gallery::TestGallery;
 /// Renders the home page of your application.
 #[component]
 pub fn HomePage() -> impl IntoView {
-    // Creates a reactive value to update the button
-    let count = RwSignal::new(0);
 
-    let on_click = move |_| *count.write() += 1;
 
     let model = use_context::<RwSignal<AppModel>>().expect("no model?");
     let robots = LocalResource::new(|| fetch_robots());
 
     view! {
-        <Stylesheet id="leptos" href="/home_page.scss" />
-        <button on:click=on_click>"Click Me: " {move || count.get()}</button>
         <section id="gallery">
             <Suspense fallback=|| {
                 view! { <p>"Loading..."</p> }
