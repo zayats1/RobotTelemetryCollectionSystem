cc0799a052e450e9d4ed98812758b8fd8cc0e855
zayats1 <zaiats.bogdan@gmail.com>
2026-04-07 11:01:21 +0300

    tiny fix

--- server/src/bin/robot_info_provider.rs
+++ server/src/bin/robot_info_provider.rs
@@ -5,9 +5,8 @@ use axum::{
     routing::get,
     Router,
 };
-use axum::routing::post;
 
-use server::receiver::receive_telemetry;
+use server::sender::get_telemetry_for_id;
 use tracing::{info};
 use tracing_subscriber;
 use server::AppState;
@@ -21,14 +20,14 @@ async fn main()  -> Result<(), std::io::Error> {
         .build()
         .await.expect("Failed to initialize database");
     let app = Router::new().route("/", get(|| async { "telemetry collector" }))
-        .route("/telemetry",post(receive_telemetry))
+        .route("/info_from/", get(get_telemetry_for_id))
         .with_state(AppState{db:Arc::new(db)});
 
     // run our app with hyper, listening globally on port 3000
 
 
     info!("The server is starting");
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3020").await?;
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
 
     axum::serve(listener, app).await
 }
